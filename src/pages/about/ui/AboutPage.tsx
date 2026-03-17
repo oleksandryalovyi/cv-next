956542415dbba95d68f297ea23d5c77dd86116fa
Oleksandr Yalovyi <yalovyialex@gmail.com>
2026-03-17 09:54:15 +0100

    change about page text

--- src/pages/about/ui/AboutPage.tsx
+++ src/pages/about/ui/AboutPage.tsx
@@ -3,8 +3,7 @@ export function AboutPage() {
     <div className="pt-70 text-center text-xl">
       <h1>Hello! My name is Oleksandr Yalovyi</h1>
       <p>I am a Fullstack developer with over than 7 years of experience</p>
-      <p>Navigate in the website using arrow keys</p>
-      <p>Fill free to ping me in LinkedIn or write a letter</p>
+      <p>Feel free to ping me in LinkedIn or write a letter</p>
     </div>
   );
 }
